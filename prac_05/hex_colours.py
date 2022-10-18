79ca2e436ebac0075832129744a63dd77e4f1cfe
ToxicImpulse <carl.estranero@gmail.com>
2022-10-18 18:00:05 +1000

    Completed prac 5 hex colours exercise

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 Practical
+This is a program that allows you to look up hexadecimal colour codes like those at http://www.color-hex.com/color-names.html
+"""
+NAME_TO_HEX = {"absolute Zero": "#0048ba", "acid Green": "#b0bf1a", "aliceblue": "#f0f8ff",
+               "alizarin crimson": "#e32636", "amaranth": "#e52b50", "amber": "#ffbf00",
+               "amethyst": "#9966cc", "apricot": "#fbceb1", "aqua": "#00ffff", "aquamarine1": "#7fffd4"}
+
+name = input("Enter colour name: ")
+while name.lower() != "":
+    try:
+        print(NAME_TO_HEX[name])
+    except KeyError:
+        print("Invalid colour name")
+    name = input("Enter colour name: ")
