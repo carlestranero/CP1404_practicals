867c70351787b3b5244b75e12364f3d62dd34241
ToxicImpulse <carl.estranero@gmail.com>
2022-11-23 18:45:15 +1000

    Finished modificaitons to taxi.py

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,7 +4,7 @@ CP1404 Practical - Carl Estranero - Taxi Test
 
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi = Taxi("Prius 1", 100)
 my_taxi.drive(40)
 print(my_taxi)
 print(f"Taxi fare is ${my_taxi.get_fare()}")
